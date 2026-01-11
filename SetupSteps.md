cad47463ae1290abbb6af110a675ae24f2882e34
Sa9m <abyssalflerken@gmail.com>
2026-01-11 15:49:09 -0600

    Runs blink on the Pico2

---
+++ SetupSteps.md
@@ -0,0 +1,10 @@
+## Setup 
+This is just what I did to get this all setup for running.
+Keep in mind that I am on NixOS (Linux) and so the setup might not be exactly the same for someone on Windows.
+
+
+First, I installed Pico tool and probe-rs. Note that I don't think we will likely be using probe-rs right now, but we might in the future.
+
+Then I cloned the Pico-sdk, but it is already in the repo, so you don't need to do that again.
+
+Now, we need to build the pico-sdk and the picotool SDK. You shouldn't need to do that either, though, because I already have.
---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Trantor Flight Code - Pico 2 Dev Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      system = "x86_64-linux";
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "rust-analyzer" ];
+        targets = [ "thumbv8m.main-none-eabihf" ];
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustToolchain
+          
+          probe-rs-tools 
+          elf2uf2-rs
+          
+          pkg-config
+          udev
+        ];
+
+        shellHook = ''
+          echo "Target: thumbv8m.main-none-eabihf (Pico 2 / RP2350)"
+          echo "Rust: $(rustc --version)"
+        '';
+      };
+    };
+}
