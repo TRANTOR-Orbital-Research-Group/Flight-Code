bcba50e2d565a886484466ae6c0455a988586a58
Logan Rilling <38445662+LoganRilling@users.noreply.github.com>
2026-02-01 16:17:40 -0600

    Redireted main into examples as the file "sd_write.rs"

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,33 @@
 #![no_std]
 #![no_main]
-
 use embedded_hal::delay::DelayNs;
-use hal::block::ImageDef;
-use panic_halt as _;
 use rp235x_hal::{self as hal, Clock};
-
-use usb_device::{class_prelude::*, prelude::*};
-use usbd_serial::SerialPort;
-
-use hal::fugit::RateExtU32;
-use heapless::String;
-
-use core::fmt::Write;
-
+// For SPI
+use embassy_rp::spi;
+use embassy_rp::spi::Spi;
+use embassy_time::Delay;
 use embedded_hal_bus::spi::ExclusiveDevice;
+
+// For CS Pin
+use embassy_rp::gpio::{Level, Output};
+
+// For SdCard
 use embedded_sdmmc::{SdCard, TimeSource, Timestamp, VolumeIdx, VolumeManager};
+
+use panic_halt as _;
+use rp235x_hal::block::ImageDef;
+use rp235x_hal::fugit::RateExtU32;
+use usb_device::bus::UsbBusAllocator;
+use usb_device::device::{StringDescriptors, UsbDeviceBuilder, UsbVidPid};
+use usbd_serial::SerialPort;
 
 #[unsafe(link_section = ".start_block")]
 #[used]
 pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
 
-const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+pub const XTAL_FREQ_HZ: u32 = 12_000_000u32;
 
+/// Code from https://github.com/rp-rs/rp-hal-boards/blob/main/boards/rp-pico/examples/pico_spi_sd_card.rs
 /// A dummy timesource, which is mostly important for creating files.
 #[derive(Default)]
 pub struct DummyTimesource();
@@ -41,6 +46,7 @@ impl TimeSource for DummyTimesource {
         }
     }
 }
+
 
 #[hal::entry]
 fn main() -> ! {
@@ -61,32 +67,13 @@ fn main() -> ! {
     let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
 
     let sio = hal::Sio::new(pac.SIO);
+
     let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
-
-    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
-        pac.USB,
-        pac.USB_DPRAM,
-        clocks.usb_clock,
-        true,
-        &mut pac.RESETS,
-    ));
-
-    let mut serial = SerialPort::new(&usb_bus);
-
-    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-        .strings(&[StringDescriptors::default()
-            .manufacturer("implRust")
-            .product("Ferris")
-            .serial_number("TEST")])
-        .unwrap()
-        .device_class(2) // 2 for the CDC, from: https://www.usb.org/defined-class-codes
-        .build();
-
     let spi_cs = pins.gpio1.into_push_pull_output();
     let spi_sck = pins.gpio2.into_function::<hal::gpio::FunctionSpi>();
     let spi_mosi = pins.gpio3.into_function::<hal::gpio::FunctionSpi>();
@@ -99,60 +86,64 @@ fn main() -> ! {
         400.kHz(), // card initialization happens at low baud rate
         embedded_hal::spi::MODE_0,
     );
-
     let spi = ExclusiveDevice::new(spi, spi_cs, timer).unwrap();
-    let sdcard = SdCard::new(spi, timer);
-    let mut buff: String<64> = String::new();
+
+    let mut config = spi::Config::default();
+    config.frequency = 400_000;
+
+    let sdcard = SdCard::new(spi, Delay);
+
+    //Read SD card size to verify initialization
+    log::info!("Init SD card controller and retrieve card size...");
+    let sd_size = sdcard.num_bytes().expect("failed to get sdcard size");
+    log::info!("card size is {} bytes", sd_size);
+
 
     let mut volume_mgr = VolumeManager::new(sdcard, DummyTimesource::default());
+    let mut volume0 = volume_mgr
+        .open_volume(VolumeIdx(0))
+        .expect("failed to open volume");
 
-    let mut is_read = false;
+    let mut root_dir = volume0.open_root_dir().expect("failed to open root dir");
+
+    //Get the file to open for reading. In this case, it is RUST.TXT
+    let mut read_file = root_dir
+        .open_file_in_dir("RUST.TXT", embedded_sdmmc::Mode::ReadOnly)
+        .expect("failed to open RUST.TXT file");
+
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USB,
+        pac.USB_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    let mut serial = SerialPort::new(&usb_bus);
+
+    /*let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .strings(&[StringDescriptors::default()
+            .manufacturer("implRust")
+            .product("Ferris")
+            .serial_number("TEST")])
+        .unwrap()
+        .device_class(2) // 2 for the CDC, from: https://www.usb.org/defined-class-codes
+        .build();*/
+
     loop {
-        let _ = usb_dev.poll(&mut [&mut serial]);
-        if !is_read && timer.get_counter().ticks() >= 2_000_000 {
-            is_read = true;
-            serial
-                .write("Init SD card controller and retrieve card size...".as_bytes())
-                .unwrap();
-            match volume_mgr.device().num_bytes() {
-                Ok(size) => {
-                    write!(buff, "card size is {} bytes\r\n", size).unwrap();
-                    serial.write(buff.as_bytes()).unwrap();
-                }
-                Err(e) => {
-                    write!(buff, "Error: {:?}", e).unwrap();
-                    serial.write(buff.as_bytes()).unwrap();
+        while !read_file.is_eof() {           //Loop until the end of the file
+            break; //Skip for now
+            let mut buffer = [0u8; 32]; //Print size
+
+            if let Ok(n) = read_file.read(&mut buffer) {
+                if let Ok(s) = core::str::from_utf8(&buffer[..n]) {
+                    serial.write(s.as_bytes()).unwrap(); //Output string s from buffer
+                } else {
+                    serial.write(&buffer[..n]).unwrap(); //Output final string from buffer
                 }
             }
-            buff.clear();
-
-            let Ok(mut volume0) = volume_mgr.open_volume(VolumeIdx(0)) else {
-                let _ = serial.write("err in open_volume".as_bytes());
-                continue;
-            };
-
-            let Ok(mut root_dir) = volume0.open_root_dir() else {
-                serial.write("err in open_root_dir".as_bytes()).unwrap();
-                continue;
-            };
-
-            let Ok(mut my_file) =
-                root_dir.open_file_in_dir("RUST.TXT", embedded_sdmmc::Mode::ReadOnly)
-            else {
-                serial.write("err in open_file_in_dir".as_bytes()).unwrap();
-                continue;
-            };
-
-            while !my_file.is_eof() {
-                let mut buffer = [0u8; 32];
-                let num_read = my_file.read(&mut buffer).unwrap();
-                for b in &buffer[0..num_read] {
-                    write!(buff, "{}", *b as char).unwrap();
-                }
-            }
-            serial.write(buff.as_bytes()).unwrap();
         }
-        buff.clear();
+        serial.write("Loop!".as_bytes()).unwrap();
 
         timer.delay_ms(50);
     }
