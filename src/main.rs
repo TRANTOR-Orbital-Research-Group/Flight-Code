bf264944a4bda953a6be08c7f3b7868fe212e486
Logan Rilling <38445662+LoganRilling@users.noreply.github.com>
2026-02-09 18:05:02 -0600

    Starting over the code

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 #![no_std]
 #![no_main]
-use embedded_hal::delay::DelayNs;
-use rp235x_hal::{self as hal, Clock};
+
+use rp235x_hal as hal;
+use hal::block::ImageDef;
+use rp235x_hal::{spi, Clock};
+use embedded_hal::digital::OutputPin;
+use {panic_probe as _};
+use defmt_rtt as _;
 use embedded_hal_bus::spi::ExclusiveDevice;
-
-// For SdCard
 use embedded_sdmmc::{SdCard, TimeSource, Timestamp, VolumeIdx, VolumeManager};
-
-use panic_halt as _;
-use rp235x_hal::block::ImageDef;
 use rp235x_hal::fugit::RateExtU32;
-use usb_device::bus::UsbBusAllocator;
-use usb_device::device::{StringDescriptors, UsbDeviceBuilder, UsbVidPid};
-use usbd_serial::SerialPort;
 
+/// Tell the Boot ROM about our application
 #[unsafe(link_section = ".start_block")]
 #[used]
 pub static IMAGE_DEF: ImageDef = hal::block::ImageDef::secure_exe();
+/// External high-speed crystal on the Raspberry Pi Pico 2 board is 12 MHz.
+/// Adjust if your board has a different frequency
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+// USB Device support
+use usb_device::{class_prelude::*, prelude::*};
+// USB Communications Class Device support
+use usbd_serial::SerialPort;
 
-pub const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+
 
 /// Code from https://github.com/rp-rs/rp-hal-boards/blob/main/boards/rp-pico/examples/pico_spi_sd_card.rs
 /// A dummy timesource, which is mostly important for creating files.
@@ -43,9 +49,15 @@ impl TimeSource for DummyTimesource {
 
 #[hal::entry]
 fn main() -> ! {
+    // Grab our singleton objects
     let mut pac = hal::pac::Peripherals::take().unwrap();
+
+    // Set up the watchdog driver - needed by the clock setup code
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
+    // Configure the clocks
+    //
+    // The default is to generate a 125 MHz system clock
     let clocks = hal::clocks::init_clocks_and_plls(
         XTAL_FREQ_HZ,
         pac.XOSC,
@@ -55,18 +67,45 @@ fn main() -> ! {
         &mut pac.RESETS,
         &mut watchdog,
     )
-    .ok()
-    .unwrap();
-    let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
+        .ok()
+        .unwrap();
 
+    // The single-cycle I/O block controls our GPIO pins
     let sio = hal::Sio::new(pac.SIO);
 
+    // Set the pins up according to their function on this particular board
     let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
+
+    let mut timer = hal::Timer::new_timer0(pac.TIMER0, &mut pac.RESETS, &clocks);
+    let mut timer_sd = timer;
+
+    // Creating a usb bus to use
+    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
+        pac.USB,
+        pac.USB_DPRAM,
+        clocks.usb_clock,
+        true,
+        &mut pac.RESETS,
+    ));
+
+    // Creating a Serial port on top of the usb bus
+    let mut serial = SerialPort::new(&usb_bus);
+
+    // Creating the device based on the serial port based on the usb bus
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .strings(&[StringDescriptors::default()
+            .manufacturer("implRust")
+            .product("Ferris")
+            .serial_number("TEST")])
+        .unwrap()
+        .device_class(2) // 2 for the CDC, from: https://www.usb.org/defined-class-codes
+        .build();
+
     let spi_cs = pins.gpio1.into_push_pull_output();
     let spi_sck = pins.gpio2.into_function::<hal::gpio::FunctionSpi>();
     let spi_mosi = pins.gpio3.into_function::<hal::gpio::FunctionSpi>();
@@ -79,62 +118,49 @@ fn main() -> ! {
         400.kHz(), // card initialization happens at low baud rate
         embedded_hal::spi::MODE_0,
     );
-    let spi = ExclusiveDevice::new(spi, spi_cs, timer).unwrap();
+    let spi = ExclusiveDevice::new(spi, spi_cs, &mut timer).unwrap();
 
-    let sdcard = SdCard::new(spi, timer);
+    let sdcard = SdCard::new(spi, &mut timer_sd);
 
-    //Read SD card size to verify initialization
-    log::info!("Init SD card controller and retrieve card size...");
-    let sd_size = sdcard.num_bytes().expect("failed to get sdcard size");
-    log::info!("card size is {} bytes", sd_size);
+    let sd_size = sdcard.num_bytes();
 
+    // Getting the LED pin ready
+    let mut led = pins.gpio25.into_push_pull_output();
 
     let mut volume_mgr = VolumeManager::new(sdcard, DummyTimesource::default());
+
+    //While this block is in the code, the associated COM port is not readable
     let mut volume0 = volume_mgr
         .open_volume(VolumeIdx(0))
-        .expect("failed to open volume");
+        .unwrap();
+    ////////////
 
-    let mut root_dir = volume0.open_root_dir().expect("failed to open root dir");
+    //let mut root_dir = volume0.open_root_dir().expect("failed to open root dir");
+    let mut ticks = 0;
 
-    //Get the file to open for reading. In this case, it is RUST.TXT
-    let mut read_file = root_dir
-        .open_file_in_dir("RUST.TXT", embedded_sdmmc::Mode::ReadOnly)
-        .expect("failed to open RUST.TXT file");
+    loop{
+        // Updating the ticks
+        ticks += 1;
 
-    let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
-        pac.USB,
-        pac.USB_DPRAM,
-        clocks.usb_clock,
-        true,
-        &mut pac.RESETS,
-    ));
+        // Responding if it hasn't said hello
+        if ticks > 1_000_000 {
+            // Writes bytes from `data` into the port and returns the number of bytes written.
+            let _ = serial.write(b"Hello, Rust!\r\n");
+            ticks -= 1_000_000;
+        }
 
-    let mut serial = SerialPort::new(&usb_bus);
-
-    /*let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-        .strings(&[StringDescriptors::default()
-            .manufacturer("implRust")
-            .product("Ferris")
-            .serial_number("TEST")])
-        .unwrap()
-        .device_class(2) // 2 for the CDC, from: https://www.usb.org/defined-class-codes
-        .build();*/
-
-    loop {
-        while !read_file.is_eof() {           //Loop until the end of the file
-            break; //Skip for now
-            let mut buffer = [0u8; 32]; //Print size
-
-            if let Ok(n) = read_file.read(&mut buffer) {
-                if let Ok(s) = core::str::from_utf8(&buffer[..n]) {
-                    serial.write(s.as_bytes()).unwrap(); //Output string s from buffer
-                } else {
-                    serial.write(&buffer[..n]).unwrap(); //Output final string from buffer
+        if usb_dev.poll(&mut [&mut serial]) {
+            let mut buf = [0u8; 64];
+            if let Ok(count) = serial.read(&mut buf) {
+                for &byte in &buf[..count] {
+                    if byte == b'r' {
+                        led.set_high().expect("unable to turn on LED");
+                    } else {
+                        led.set_low().expect("unable to turn off LED");
+                    }
                 }
             }
         }
-        serial.write("Loop!".as_bytes()).unwrap();
 
-        timer.delay_ms(50);
     }
 }
